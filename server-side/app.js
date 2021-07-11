103d188e4f39570750dc93ba27caa5ca17bf65ef
mastabiyu <mastabiyu@gmail.com>
2021-07-11 09:50:17 -0500

    updated app.js

--- server-side/app.js
+++ server-side/app.js
@@ -12,12 +12,20 @@ app.use(express.json());
 
 
 
+app.use(authRouter);
+app.use('/books', bookRouter);
 
+app.use((req, res, next) => {
+    res.status(404).json({ error: req.url + ' API not supported!' });
+});
 
-
-
-
-
+app.use((err, req, res, next) => {
+    if (err.message === 'NOT Found') {
+        res.status(404).json({ error: err.message });
+    } else {
+        res.status(500).json({ error: 'Something is wrong! Try later' });
+    }
+});
 
 
 
