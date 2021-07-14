0d0f34430c6ff888dfba292460de776ac96d1500
mastabiyu <mastabiyu@gmail.com>
2021-07-13 23:52:13 -0500

    updated carts

---
+++ server-side/routes/cartRouters.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cartController = require('../controllers/cartController');
+
+const cartRouter = express.Router();
+
+cartRouter.post('/cart/:id', cartController.addToCart);
+cartRouter.delete('/cart/:id', cartController.deleteProduct);
+cartRouter.get('/', cartController.getAll);
+cartRouter.get('/', cartController.isEmpty);
+
+
+module.exports = cartRouter;
+
